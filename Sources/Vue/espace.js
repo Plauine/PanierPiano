3ffa8b5186a588ce096e286fb2f98811171c41b5
Pauline BAUDOUIN <paulinebaudouin2@gmail.com>
2017-12-27 09:53:13 +0100

    Page espace terminée

---
+++ Sources/Vue/espace.js
@@ -0,0 +1,14 @@
+function changeValues(){
+	$("input").prop("readonly", false);
+	$("#modifier").hide();
+	$("#valider").css("visibility", "visible");
+	$("#annuler").css("visibility", "visible");
+}
+
+function validerForm(){
+
+}
+
+function cancelChanges(){
+
+}
